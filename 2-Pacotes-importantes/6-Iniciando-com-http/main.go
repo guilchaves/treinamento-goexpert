99addd12db6e5aad45ee7a2f56a1ce25ca291dcd
guilherme chaves <chaves.guilherme@proton.me>
2025-02-04 23:32:49 -0300

    chore: adiciona aulas de http, mux e file server

---
+++ 2-Pacotes-importantes/6-Iniciando-com-http/main.go
@@ -0,0 +1,73 @@
+// INFO: 2.6-Iniciando com HTTP | 2.7-Manipulando headers
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Unidade     string `json:"unidade"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Estado      string `json:"estado"`
+	Regiao      string `json:"regiao"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+func main() {
+	http.HandleFunc("/", FetchCEPHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func FetchCEPHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	cepParam := r.URL.Query().Get("cep")
+	if cepParam == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	cep, err := FetchCep(cepParam)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+    json.NewEncoder(w).Encode(cep)
+}
+
+func FetchCep(cep string) (*ViaCEP, error) {
+	resp, err := http.Get("https://viacep.com.br/ws/" + cep + "/json/")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var c ViaCEP
+	err = json.Unmarshal(body, &c)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
---
+++ 2-Pacotes-importantes/7-Mux/main.go
@@ -0,0 +1,26 @@
+// INFO: 2.8- Mux
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	//mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//	w.Write([]byte("Hello world"))
+	//})
+	mux.HandleFunc("/", HomeHandler)
+    mux.Handle("/blog", blog{title: "My Blog"})
+	http.ListenAndServe(":8080", mux)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world"))
+}
+
+type blog struct {
+    title string
+}
+
+func (b blog) ServeHTTP(w http.ResponseWriter, r *http.Request){
+    w.Write([]byte(b.title))
+}
