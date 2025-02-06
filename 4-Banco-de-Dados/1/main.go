07d463dcbe6ae61df29f68128eca805d33eff54e
guilherme chaves <chaves.guilherme@proton.me>
2025-02-06 17:56:08 -0300

    chore: adiciona modulo de banco de dados

---
+++ 4-Banco-de-Dados/1/main.go
@@ -0,0 +1,151 @@
+// INFO: 4.3-Inserindo dados | 4.4-Alterando dados | 4.5-Query rows | 4.6-Selecionando mais de um produto | 4.7-Deletando produto
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func NewProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.NewString(),
+		Name:  name,
+		Price: price,
+	}
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	product := NewProduct("Notebook", 1899.90)
+	product2 := NewProduct("Nintendo Switch", 2399.90)
+	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+
+	err = insertProduct(db, product2)
+	if err != nil {
+		panic(err)
+	}
+
+	product.Price = 100.0
+	err = updateProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+
+	//p, err := selectProductById(db, product.ID)
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	//fmt.Printf("Product: %v, possui o preço de R$%.2f\n", p.Name, p.Price)
+
+    products, err := selectFindAllProducts(db)
+    if err != nil {
+        panic(err)
+    }
+
+    for _, p := range products {
+        fmt.Printf("Product: %v, possui o preço de R$%.f\n", p.Name, p.Price)
+    }
+
+    err = deleteProductById(db, product.ID)
+    if err != nil {
+        panic(err)
+    }
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("insert into products(id, name, price) values(?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("update products set name = ?, price = ? where id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func selectProductById(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("select id, name, price from products where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	var p Product
+	err = stmt.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		return nil, err
+	}
+
+	return &p, nil
+}
+
+func selectFindAllProducts(db *sql.DB) ([]Product, error) {
+	rows, err := db.Query("select id, name, price from products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []Product
+	for rows.Next() {
+		var p Product
+		err = rows.Scan(&p.ID, &p.Name, &p.Price)
+		if err != nil {
+			return nil, err
+		}
+        products = append(products, p)
+	}
+
+    return products, nil
+}
+
+func deleteProductById(db *sql.DB, id string) error {
+    stmt, err := db.Prepare("delete from products where id = ?")
+    if err != nil {
+        return err
+    }
+    defer stmt.Close()
+
+    _, err = stmt.Exec(id)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
